5d534396c85b2de8c450e7fad3841c634780af95
Your Name <you@example.com>
2020-02-04 02:03:50 +0300

    add .travis.yml

--- hw1-CLI/test/Spec.hs
+++ hw1-CLI/test/Spec.hs
@@ -1,58 +1,49 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 import           Test.Hspec
 import           Test.Hspec.QuickCheck
 import           Test.QuickCheck
 import           Parser
-import           Data.List
+import           Substitution
+import           Shell
+import           Control.Monad.Identity
 
-genVariableName :: Gen String
-genVariableName = listOf1 $ elements ['a'..'z']
+newtype IdEnv a = IdEnv (Identity a) deriving (Functor, Applicative, Monad, Eq, Show)
 
-genValidWord :: Gen String
-genValidWord = listOf1 $ elements ['a'..'z']
+instance ShellEnv IdEnv where
+    loadVar var = pure var
+    storeVar var val = return ()
+    getCurrentDirectory = undefined 
 
-genTokenPart :: Gen TokenPart
-genTokenPart = oneof [CharSequence  <$> genValidWord, StrongQuoting <$> genValidWord, WeakQuoting  <$> genValidWord]
-
-genToken :: Gen Token
-genToken = Token <$> (shrink <$> listOf1 genTokenPart)
-    where
-        shrink [] = []
-        shrink [x] = [x]
-        shrink ((CharSequence s1):(CharSequence s2):rest) = shrink $ (CharSequence (s1 ++ s2)):rest
-        shrink (x:rest) = x:(shrink rest)
-
-genAssignment :: Gen Expression
-genAssignment = do 
-    var <- genVariableName
-    val <- genToken
-    return $ Assignment var val
-
-genPipe :: Gen Expression
-genPipe = Pipe <$> listOf (listOf1 genToken)
-
-genExpression :: Gen Expression
-genExpression = oneof [genAssignment, genPipe]
-
-tokenToString :: Token -> String
-tokenToString (Token t) = concat $ map convert t
-    where
-        convert (CharSequence s) = s
-        convert (WeakQuoting s) = "\"" ++ s ++ "\""
-        convert (StrongQuoting s) = "'" ++ s ++ "'"
-
-expressionToString :: Expression -> String
-expressionToString (Assignment var val) = var ++ "=" ++ tokenToString val
-expressionToString (Pipe ts) = intercalate " | " (map tokenListToString ts)
-    where
-        tokenListToString l = intercalate " " (map tokenToString l)
-
-prop_parseExpression = forAll genExpression $ \e -> parseExpression (expressionToString e) == Right e
+oneWord :: String -> IdEnv [String]
+oneWord s = pure [s]
 
 main :: IO ()
 main = hspec $ do
-    describe "Test numbers" $ do
-        it "comares 2 and 2" $ do
-            2 `shouldBe` 2
+    describe "Test substitution" $ do
+        it "does not substitute strong quoting" $ do
+            let wrap s = [Token [StrongQuoting s]]
+            applySubstitution (wrap "$x") `shouldBe` oneWord "$x"
+            applySubstitution (wrap "$x$x") `shouldBe` oneWord "$x$x"
+            applySubstitution (wrap "$x   $y") `shouldBe` oneWord "$x   $y"
+            applySubstitution (wrap "$x y") `shouldBe` oneWord "$x y"
+        it "does substitute weak quoting" $ do
+            let wrap s = [Token [WeakQuoting s]]
+            applySubstitution (wrap "$x") `shouldBe` oneWord "x"
+            applySubstitution (wrap "$x $y") `shouldBe` oneWord "x y"
+            applySubstitution (wrap "$x $x") `shouldBe` oneWord "x x"
+            applySubstitution (wrap "$x$y") `shouldBe` oneWord "xy"
+            applySubstitution (wrap "$x   $y") `shouldBe` oneWord "x   y"
+            applySubstitution (wrap "$xyz") `shouldBe` oneWord "xyz"
+            applySubstitution (wrap "$x z") `shouldBe` oneWord "x z"
+        it "does substitute unqouted strings" $ do
+            let wrap s = [Token [CharSequence s]]
+            applySubstitution (wrap "$x") `shouldBe` oneWord "x"
+            applySubstitution (wrap "$x $y") `shouldBe` oneWord "x y"
+            applySubstitution (wrap "$x $x") `shouldBe` oneWord "x x"
+            applySubstitution (wrap "$x$y") `shouldBe` oneWord "xy"
+            applySubstitution (wrap "$x   $y") `shouldBe` oneWord "x   y"
+            applySubstitution (wrap "$xyz") `shouldBe` oneWord "xyz"
+            applySubstitution (wrap "$x z") `shouldBe` oneWord "x z"
     describe "parsing tests" $ do
         it "parse expression" $ 
-            property prop_parseExpression  
+            property prop_parseExpression
